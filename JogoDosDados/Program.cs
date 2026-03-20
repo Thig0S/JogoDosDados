1a6ded1c22ada16009ec038ab7bac3c90ec2f328
thig0S_ <thiagokovalski5@gmail.com>
2026-03-20 13:52:01 -0300

    adicionado logica inicial do jogo

--- JogoDosDados/Program.cs
+++ JogoDosDados/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace JogoDosDados;
+﻿using System.Security.Cryptography;
+
+namespace JogoDosDados;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            System.Console.WriteLine("---------------");
+            System.Console.WriteLine("Jogo dos Dados!");
+            System.Console.WriteLine("---------------");
+
+            System.Console.Write("Digite ENTER para lancar um dado: ");
+            Console.ReadLine();
+            int resultado = RandomNumberGenerator.GetInt32(1,7);
+
+            System.Console.WriteLine("---------------");
+            System.Console.WriteLine($"O numero sorteado foi: {resultado}");
+            System.Console.WriteLine("---------------");
+
+            System.Console.WriteLine("Deseja continuar? (S/N)");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if(opcaoContinuar != "S")
+                break;
+
+            
+        }
     }
 }
